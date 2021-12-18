847e6c7239b364c407d3985b445e1ec1440165cc
JoseRodriguez-glitched <jr70084333@gmail.com>
2021-12-17 18:43:18 -0600

    update scripts

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res)=>{
   res.sendFile(__dirname+'/templates/verify.html');
 });
 
-server.listen(5000, ()=>{
+server.listen(port, ()=>{
   console.log('Server running on port *:5000');
 });
 
